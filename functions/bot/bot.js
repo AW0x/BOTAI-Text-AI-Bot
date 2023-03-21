0f532d772bc18d3e164aca4ef32e1372ad0b7121
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-21 10:49:07 +0700

    fix handler aws

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -99,7 +99,7 @@ bot.help((ctx) => {
 });
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
-exports.handler = async function (event) {
+exports.handler = async (event) => {
   try {
     await bot.handleUpdate(JSON.parse(event.body));
     return { statusCode: 200, body: "" };
