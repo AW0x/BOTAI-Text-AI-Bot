b2c490cb6cf935f5a2e81ff503aa0339490c6767
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-21 08:34:51 +0700

    Add New Bot

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
-// Dependencies
-const express = require("express");
-const app = express();
-const axios = require("axios");
-const bodyParser = require("body-parser");
-const port = 80;
-const url = "https://api.telegram.org/bot";
-const apiToken = "{6043963401:AAHBL-H9afpohGbMb9i6d7JQPicln4ZJg-o}";
-// Configurations
-app.use(bodyParser.json());
-// Endpoints
-app.post("/", (req, res) => {
-  // console.log(req.body);
-  const chatId = req.body.message.chat.id;
-  const sentMessage = req.body.message.text;
-  // Regex for hello
-  if (sentMessage.match(/hello/gi)) {
-    axios
-      .post(`${url}${apiToken}/sendMessage`, {
-        chat_id: chatId,
-        text: "hello back 👋",
-      })
-      .then((response) => {
-        res.status(200).send(response);
-      })
-      .catch((error) => {
-        res.send(error);
-      });
-  } else {
-    // if no hello present, just respond with 200
-    res.status(200).send({});
+const TelegramBot = require("node-telegram-bot-api");
+const ogs = require("open-graph-scraper");
+
+// Bot config
+const token = "6043963401:AAHBL-H9afpohGbMb9i6d7JQPicln4ZJg-o";
+const bot = new TelegramBot(token, { polling: true });
+
+let clientMessageRequest;
+
+bot.on("message", (msg) => {
+  let hi = "hi";
+  if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+    bot.sendMessage(msg.chat.id, "Hi, user Name");
+  }
+
+  let bye = "bye";
+  if (msg.text.toString().toLowerCase().includes(bye)) {
+    bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
   }
 });
-// Listening
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+
+// Reply to /ask
+bot.onText(/\/ask (.+)/, (msg, match) => {
+  clientMessageRequest = match[1];
+  bot.sendMessage(msg.chat.id, "Hi, anda telah mencoba command /ask");
 });
