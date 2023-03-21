ad102069c85ea129f089b0eed7bca83d41bc7459
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-21 10:32:38 +0700

    Fix Code

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -26,20 +26,20 @@ bot.start((ctx) => {
 });
 
 // Reply to user who run command "hi"
-bot.command("hi", async (ctx) => {
-  if (ctx.text.toString().toLowerCase().indexOf(ctx) === 0) {
-    console.log("Received 'hi' command from user ");
-    return await ctx.reply(ctx.chat.id, "Hi, user Name");
-  }
-});
+// bot.command("hi", async (ctx) => {
+//   if (ctx.text.toString().toLowerCase().indexOf(ctx) === 0) {
+//     console.log("Received 'hi' command from user ");
+//     return await ctx.reply(ctx.chat.id, "Hi, user Name");
+//   }
+// });
 
-// Reply to user who run command "bye"
-bot.command("bye", async (ctx) => {
-  if (ctx.text.toString().toLowerCase().includes(ctx)) {
-    console.log("Received 'bye' command from user ");
-    return await ctx.reply(ctx.chat.id, "Hope to see you around again , Bye");
-  }
-});
+// // Reply to user who run command "bye"
+// bot.command("bye", async (ctx) => {
+//   if (ctx.text.toString().toLowerCase().includes(ctx)) {
+//     console.log("Received 'bye' command from user ");
+//     return await ctx.reply(ctx.chat.id, "Hope to see you around again , Bye");
+//   }
+// });
 
 // Reply to user who run command /ask
 bot.command("ask", async (ctx) => {
@@ -97,5 +97,19 @@ bot.help((ctx) => {
     "This bot can perform the following command \n /image -> to create image from text \n /ask -> ank anything from me "
   );
 });
+
+// AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
+exports.handler = async (event) => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body));
+    return { statusCode: 200, body: "" };
+  } catch (e) {
+    console.error("error in handler:", e);
+    return {
+      statusCode: 400,
+      body: "This endpoint is meant for bot and telegram communication",
+    };
+  }
+};
 
 bot.launch();
