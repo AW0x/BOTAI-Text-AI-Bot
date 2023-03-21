4895d03d6dc2e24739d783d13815c43f90cf96db
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-21 11:39:17 +0700

    Fix Code

--- functions/bot/functions.js
+++ functions/bot/functions.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: process.env.API,
+  apiKey: process.env.OPENAI_API,
 });
 
 const openai = new OpenAIApi(configuration);
