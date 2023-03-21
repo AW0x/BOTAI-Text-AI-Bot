ac1edfa5151982ec9d1646613fc2effaed6b5f5a
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-21 09:10:06 +0700

    Fix Code

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,8 +1,7 @@
 const { Telegraf } = require("telegraf");
 
 // Bot config
-const token = "6043963401:AAHBL-H9afpohGbMb9i6d7JQPicln4ZJg-o";
-const bot = new Telegraf(token, { polling: true });
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 let clientMessageRequest;
 
@@ -38,3 +37,17 @@ bot.onText(/\/ask (.+)/, (msg, match) => {
   console.log("Received '/ask' command from user ");
   bot.sendMessage(msg.chat.id, "Hi, anda telah mencoba command /ask");
 });
+
+// AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
+exports.handler = async (event) => {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body));
+    return { statusCode: 200, body: "" };
+  } catch (e) {
+    console.error("error in handler:", e);
+    return {
+      statusCode: 400,
+      body: "This endpoint is meant for bot and telegram communication",
+    };
+  }
+};
