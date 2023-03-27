1acf7096c68c73c7621f7e7e188467b9d17b4d1a
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-27 16:41:34 +0700

    Update Bot

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,73 +1,39 @@
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
-const { getImage, getChat } = require("../../openai/main");
+const { getResponse } = require("../../openai/main");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
   console.log("Received /start command");
   try {
-    ctx.reply("Welcome, " + ctx.message.from.username + " to BOTAI");
-    return ctx.reply("Reply to my message to start chatting.");
+    return ctx.reply("Welcome, " + ctx.message.from.username + " to BOTAI");
   } catch (e) {
     console.error("error in start action:", e);
     return ctx.reply("Error occured");
   }
 });
 
-bot.command("ask", async (ctx) => {
-  const text = ctx.message.text?.replace("/ask", "")?.trim().toLowerCase();
-
-  if (text) {
-    ctx.sendChatAction("typing");
-    const res = await getChat(text);
-    if (res) {
-      ctx.telegram.sendMessage(ctx.message.chat.id, res, {
+bot.on("message", async (ctx) => {
+  const text = ctx.message.text?.toLowerCase();
+  try {
+    if (text) {
+      ctx.sendChatAction("typing");
+      const res = await getResponse(text);
+      if (res) {
+        ctx.telegram.sendMessage(ctx.message.chat.id, res, {
+          reply_to_message_id: ctx.message.message_id,
+        });
+      }
+    } else {
+      ctx.telegram.sendMessage(ctx.message.chat.id, "Please ask anything", {
         reply_to_message_id: ctx.message.message_id,
       });
+
+      //  reply("Please ask anything after /ask");
     }
-  } else {
-    ctx.telegram.sendMessage(
-      ctx.message.chat.id,
-      "Please ask anything after /ask",
-      {
-        reply_to_message_id: ctx.message.message_id,
-      }
-    );
-
-    //  reply("Please ask anything after /ask");
+  } catch (error) {
+    return ctx.reply("Error occured");
   }
-});
-
-// Reply to user who run command /image
-bot.command("image", async (ctx) => {
-  const text = ctx.message.text?.replace("/image", "")?.trim().toLowerCase();
-
-  if (text) {
-    const res = await getImage(text);
-
-    if (res) {
-      ctx.sendChatAction("upload_photo");
-      // ctx.sendPhoto(res);
-      // ctx.telegram.sendPhoto()
-      ctx.telegram.sendPhoto(ctx.message.chat.id, res, {
-        reply_to_message_id: ctx.message.message_id,
-      });
-    }
-  } else {
-    ctx.telegram.sendMessage(
-      ctx.message.chat.id,
-      "You have to give some description after /image",
-      {
-        reply_to_message_id: ctx.message.message_id,
-      }
-    );
-  }
-});
-
-bot.help((ctx) => {
-  ctx.reply(
-    "This bot can perform the following command \n /image -> to create image from text \n /ask -> ank anything from me "
-  );
 });
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
@@ -83,3 +49,12 @@ exports.handler = async (event) => {
     };
   }
 };
+
+bot
+  .launch()
+  .then(() => {
+    console.log("Bot started!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- openai/main.js
+++ openai/main.js
@@ -19,33 +19,21 @@ async function generateChatResponse(message, user) {
   return completion.data.choices[0].message.content;
 }
 
-const getImage = async (text) => {
-  try {
-    const response = await openai.createImage({
-      prompt: text,
-      n: 1,
-      size: "512x512",
-    });
-
-    return response.data.data[0].url;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const getChat = async (text) => {
+const getResponse = async (text) => {
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: text,
       temperature: 0,
       max_tokens: 1000,
+      frequency_penalty: 0.2,
+      presence_penalty: 0,
     });
 
-    return response.data.choices[0].text;
+    return response.data.choices[0].text.trim();
   } catch (error) {
     console.log(error);
   }
 };
 
-module.exports = { generateChatResponse, getImage, getChat };
+module.exports = { generateChatResponse, getResponse };
