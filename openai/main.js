f54b1e0ef8df464a8a242d81d2870f937168d733
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-27 16:16:08 +0700

    Update Bot

--- openai/main.js
+++ openai/main.js
@@ -1,9 +1,9 @@
 const { OpenAIApi } = require("openai");
 const openai_config = require("./openai.config");
 
-async function generateChatResponse(message, user) {
-  const openai = new OpenAIApi(openai_config);
+const openai = new OpenAIApi(openai_config);
 
+async function generateChatResponse(message, user) {
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: [
@@ -19,4 +19,33 @@ async function generateChatResponse(message, user) {
   return completion.data.choices[0].message.content;
 }
 
-module.exports = { generateChatResponse };
+const getImage = async (text) => {
+  try {
+    const response = await openai.createImage({
+      prompt: text,
+      n: 1,
+      size: "512x512",
+    });
+
+    return response.data.data[0].url;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getChat = async (text) => {
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0,
+      max_tokens: 1000,
+    });
+
+    return response.data.choices[0].text;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { generateChatResponse, getImage, getChat };
