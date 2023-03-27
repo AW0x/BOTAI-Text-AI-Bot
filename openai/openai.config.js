3c186c898ce8b95a6cf5e4214001ababff98878d
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2023-03-27 16:48:01 +0700

    Update Bot

--- openai/openai.config.js
+++ openai/openai.config.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 const { Configuration } = require("openai");
 
 module.exports = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API,
 });
